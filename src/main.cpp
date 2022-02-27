66d82b3fc81e3d654244e15fae626414b2dc00ca
VictorJimenez99 <jimenezvictor99@gmail.com>
2022-02-27 17:57:27 -0600

    initial resource_pool test

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
-#include <memory>
 #include <iostream>
-#include "lib/services/GardenerDB.h"
+#include "lib/services/connection_pool.h"
 
 int main() {
-    auto handler = GardenerDB();
-    if(handler.schema_is_complete()) {
+
+    gardener_db *handler = connection_handler().get();
+
+    if (handler->schema_is_complete()) {
         std::cout << "schema is complete" << std::endl;
-    }else {
-        std::cout << "error closing app" << std::endl;
+    } else {
+        std::cout << "Error: shutting down gardener app" << std::endl;
         return -1;
     }
-
-    return 0;
+    // listen for requests and process them in a tcp socket}
 }
